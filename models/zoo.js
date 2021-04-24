1dd3b8ef623051d7f8929f3a51d3e4ed0a4eadae
Satyachowdary97 <77590874+Satyachowdary97@users.noreply.github.com>
2021-04-24 01:13:25 -0500

    final commit

--- models/zoo.js
+++ models/zoo.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose")
 const zooSchema = mongoose.Schema({
-    Name_of_the_Zoo: String,
+    Name_of_the_Zoo: {
+        type: String,
+        required: [true, "Name required"]
+    },
     Location_of_the_Zoo: String,
-    Number_of_animals_available_in_the_Zoo: Number
+    Number_of_animals_available_in_the_Zoo: {
+        type: Number,
+        min: [1000, "Min value is 1000"],
+        max: [4000, "Max value is 400"]
+    }
 })
-module.exports = mongoose.model("zoo",zooSchema)
+module.exports = mongoose.model("zoo", zooSchema)
